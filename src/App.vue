63185889e490e5d7d72b5be0bbf6f6769bbed779
anaahgarcia <anaah.garcia19@gmail.com>
2024-12-04 16:56:23 +0000

    feat(components):login

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,63 @@
 <template>
   <h1>Hello World!</h1>
   <router-link to="/dashboard">Dashboard</router-link>
+  <router-link to="/login">login</router-link>
 
   <div>
     <router-view></router-view>
   </div>
+
+  <div id="app">
+    <header>
+
+    </header>
+
+    <main>
+      <router-view></router-view>
+    </main>
+
+    <footer>
+      <p>&copy; 2024 ABR Technology. All rights reserved.</p>
+    </footer>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: 'App',
+};
+</script>
 
-<style></style>
+<style scoped>
+#app {
+  font-family: open sans-serif, sans-serif;
+  text-align: center;
+}
+
+
+.logo-container {
+  text-align: center;
+}
+
+.logo {
+  width: 150px;
+}
+
+h1 {
+  margin-top: 1rem;
+  font-size: 2rem;
+}
+
+h2 {
+  font-size: 1.2rem;
+  color: #555;
+}
+
+footer {
+  background-color: #f4f4f4;
+  padding: 1rem;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router';
-import dashboard from './components/dashboard.vue'
+import dashboard from './components/dashboard.vue';
+import login from './components/login.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/dashboard', component: dashboard},
+        {path: '/login', component: login},
     ]
 })
 const app = createApp(App);
